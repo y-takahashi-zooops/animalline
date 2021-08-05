0f8d0dd97d6810eb5199e82c58878a5d85e66a08
hoivv <hoivv@miagi-so.net>
2021-08-05 14:11:34 +0700

    crud breeds, coatcolors

---
+++ app/Customize/Controller/Test/BreedsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Customize\Controller\Test;
+
+use Customize\Entity\Breeds;
+use Customize\Form\Type\BreedsType;
+use Customize\Repository\BreedsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/test/list_breeds")
+ */
+class BreedsController extends Controller
+{
+    /**
+     * @Route("/", name="breeds_index", methods={"GET"})
+     */
+    public function index(BreedsRepository $breedsRepository): Response
+    {
+        return $this->render('test/breeds/index.html.twig', [
+            'breeds' => $breedsRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="breeds_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $breed = new Breeds();
+        $form = $this->createForm(BreedsType::class, $breed);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($breed);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('breeds_index');
+        }
+
+        return $this->render('test/breeds/new.html.twig', [
+            'breed' => $breed,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="breeds_show", methods={"GET"})
+     */
+    public function show(Breeds $breed): Response
+    {
+        return $this->render('test/breeds/show.html.twig', [
+            'breed' => $breed,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="breeds_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Breeds $breed): Response
+    {
+        $form = $this->createForm(BreedsType::class, $breed);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('breeds_index');
+        }
+
+        return $this->render('test/breeds/edit.html.twig', [
+            'breed' => $breed,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="breeds_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Breeds $breed): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $breed->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($breed);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('breeds_index');
+    }
+}
