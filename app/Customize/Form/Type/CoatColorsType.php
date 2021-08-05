df44ed12adb6a5822670da5f56b5341d51df03e6
hoivv <hoivv@miagi-so.net>
2021-08-05 15:52:10 +0700

    fix missing type

--- app/Customize/Form/Type/CoatColorsType.php
+++ app/Customize/Form/Type/CoatColorsType.php
@@ -4,6 +4,7 @@ namespace Customize\Form\Type;
 
 use Customize\Entity\CoatColors;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
