2326294ef965478d5b9f28661ee399c774559c94
Kim Lam <lamkd@miagi-so.net>
2021-08-09 11:16:41 +0700

    Add entity conservation_pet_image

---
+++ app/Customize/Form/Type/ConservationPetImageType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Customize\Form\Type;
+
+use Customize\Entity\ConservationPetImage;
+use Eccube\Common\EccubeConfig;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ConservationPetImageType extends AbstractType
+{
+    /**
+    * @var EccubeConfig
+    */
+    protected $eccubeConfig;
+
+    public function __construct(EccubeConfig $eccubeConfig)
+    {
+        $this->eccubeConfig = $eccubeConfig;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('conservation_id', IntegerType::class)
+            ->add('image_type', IntegerType::class)
+            ->add('image_uri', TextType::class, [
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => $this->eccubeConfig['eccube_stext_len'],
+                    ]),
+                    'attr' => [
+                        'maxlength' => $this->eccubeConfig['eccube_stext_len'],
+                    ],
+                ]
+            ])
+            ->add('sort_order', IntegerType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ConservationPetImage::class,
+        ]);
+    }
+}
