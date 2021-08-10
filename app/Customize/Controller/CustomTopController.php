58c4351035d5a1e4766897f8b9fee224d12d95fd
ZOOOPSJ-PC-012\m.goto <ryo.sato@zoops-japan.co.jp>
2021-08-10 16:13:17 +0900

    ペット詳細画面からお問い合わせ画面へのルート追加

--- app/Customize/Controller/CustomTopController.php
+++ app/Customize/Controller/CustomTopController.php
@@ -17,6 +17,9 @@ use Eccube\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Eccube\Form\Type\Front\ContactType;
 
 class CustomTopController extends AbstractController
 {
@@ -69,8 +72,44 @@ class CustomTopController extends AbstractController
      * @Route("/contact", name="ani_contact")
      * @Template("ani_contact.twig")
      */
-    public function contact()
+    public function contact(Request $request)
     {
-        return [];
+
+        $builder = $this->formFactory->createBuilder(ContactType::class);
+
+        // if ($this->isGranted('ROLE_ADOPTION_USER')) {
+        //     /** @var Customer $user */
+        //     $user = $this->getUser();
+        //     $builder->setData(
+        //         [
+        //             'name01' => $user->getName01(),
+        //             'name02' => $user->getName02(),
+        //             'kana01' => $user->getKana01(),
+        //             'kana02' => $user->getKana02(),
+        //             'postal_code' => $user->getPostalCode(),
+        //             'pref' => $user->getPref(),
+        //             'addr01' => $user->getAddr01(),
+        //             'addr02' => $user->getAddr02(),
+        //             'phone_number' => $user->getPhoneNumber(),
+        //             'email' => $user->getEmail(),
+        //         ]
+        //     );
+        // }
+
+        // FRONT_CONTACT_INDEX_INITIALIZE
+        $event = new EventArgs(
+            [
+                'builder' => $builder,
+            ],
+            $request
+        );
+        $this->eventDispatcher->dispatch(EccubeEvents::FRONT_CONTACT_INDEX_INITIALIZE, $event);
+
+        $form = $builder->getForm();
+        $form->handleRequest($request);      
+
+        return [
+            'form' => $form->createView(),
+        ];
     }
 }
