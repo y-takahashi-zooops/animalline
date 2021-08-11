c54325c3143621333d6d826364af92b6ecba9537
hieucv <hieucv@miagi-so.net>
2021-08-11 15:14:05 +0700

    add type contact

--- app/Customize/Form/Type/ConservationContactType.php
+++ app/Customize/Form/Type/ConservationContactType.php
@@ -13,8 +13,11 @@
 
 namespace Customize\Form\Type;
 
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Eccube\Common\EccubeConfig;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -44,13 +47,28 @@ class ConservationContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('conservation', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('message_from', ChoiceType::class, [
+                'choices' =>
+                    [
+                        'ユーザー' => 1,
+                        '保護団体' => 2,
+                    ],
+                'required' => true,
+                'expanded' => false,
+            ])
+            ->add('pet', IntegerType::class, [
+                'required' => false,
+            ])
             ->add('contact_type', ChoiceType::class, [
-                'choices' => 
-                [
-                    '問い合わせ' => 1, 
-                    '見学希望' => 2,
-                    '返信' => 3,
-                ],
+                'choices' =>
+                    [
+                        '問い合わせ' => 1,
+                        '見学希望' => 2,
+                        '返信' => 3,
+                    ],
                 'required' => true,
                 'expanded' => false,
             ])
@@ -68,6 +86,39 @@ class ConservationContactType extends AbstractType
                 'constraints' => [
                     new Assert\NotBlank(),
                 ],
+            ])
+            ->add('parent_message_id', ChoiceType::class, [
+                'choices' =>
+                    [
+                        '新規' => 0,
+                    ],
+                'required' => true,
+                'expanded' => false,
+            ])
+            ->add('send_date', DateTimeType::class, [
+                'required' => true,
+            ])
+            ->add('is_response', ChoiceType::class, [
+                'choices' =>
+                    [
+                        '未返信' => 0,
+                        '返信あり' => 1,
+                    ],
+                'required' => true,
+                'expanded' => false,
+            ])
+            ->add('contract_status', ChoiceType::class, [
+                'choices' =>
+                    [
+                        '交渉中' => 0,
+                        '成約 ' => 1,
+                        '非成約' => 2,
+                    ],
+                'required' => true,
+                'expanded' => false,
+            ])
+            ->add('reason', IntegerType::class, [
+                'required' => true,
             ]);
     }
 
