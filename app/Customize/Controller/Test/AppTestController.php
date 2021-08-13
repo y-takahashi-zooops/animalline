fb37332491f86bd356c89fb185a5264f9e6db172
quyenhn-miagi <quyenhn@miagi-so.net>
2021-08-13 11:36:52 +0700

    init commit cropper

--- app/Customize/Controller/Test/AppTestController.php
+++ app/Customize/Controller/Test/AppTestController.php
@@ -4,17 +4,30 @@ namespace Customize\Controller\Test;
 
 use Eccube\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppTestController extends AbstractController
 {
     /**
-     * @Route("/test/list_adoption", name="test_crud_adoption")
-     * @Template("test/alm_adoption.twig")
+     * @Route("/test/crop_image", name="test_crop_image")
+     * @Template("test/crop_image.twig")
      */
-    public function index()
+    public function crop()
     {
-        return [];
+        $form = $this->formFactory->createBuilder(FormType::class)
+            ->add('image', FileType::class, [
+                'label' => 'Image',
+                'attr' => [
+                    'onchange' => 'previewFile()'
+                ]
+            ])
+            ->getForm();
+        return [
+            'form' => $form->createView(),
+        ];
     }
 }
