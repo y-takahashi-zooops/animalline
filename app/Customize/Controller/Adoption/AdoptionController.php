570502645d1e1265c43f5bfca8e92337126ef26b
DESKTOP-VROH117\q_sat <kalavinka.asia@gmail.com>
2021-08-16 01:25:21 +0900

    コントローラー整理。作業指示準備

--- app/Customize/Controller/Adoption/AdoptionController.php
+++ app/Customize/Controller/Adoption/AdoptionController.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * This file is part of EC-CUBE
- *
- * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
- *
- * http://www.ec-cube.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Customize\Controller\Adoption;
 
 use Carbon\Carbon;
@@ -173,86 +162,7 @@ class AdoptionController extends AbstractController
         return;
     }
 
-    /**
-     * 保護団体管理ページTOP
-     *
-     * @Route("/adoption/configration/", name="adoption_configration")
-     * @Template("animalline/adoption/configration/index.twig")
-     */
-    public function adoption_configration(Request $request)
-    {
-        $rootMessages = $this->conservationContactsRepository->findBy(
-            [
-                'parent_message_id' => AnilineConf::ROOT_MESSAGE_ID,
-                'Conservation' => $this->getUser()
-            ],
-            ['is_response' => 'ASC', 'send_date' => 'DESC']
-        );
-
-        $lastReplies = [];
-        foreach ($rootMessages as $message) {
-            $lastReply = $this->conservationContactsRepository->findOneBy(
-                ['parent_message_id' => $message->getId()],
-                ['send_date' => 'DESC']
-            );
-            $lastReplies[$message->getId()] = $lastReply ? $lastReply->getSendDate() : null;
-        }
-
-        return $this->render(
-            'animalline/adoption/configration/index.twig',
-            [
-                'rootMessages' => $rootMessages,
-                'lastReplies' => $lastReplies,
-            ]
-        );
-    }
-
-    /**
-     * 保護団体管理ページ - 取引メッセージ履歴
-     *
-     * @Route("/adoption/configration/message/{contact_id}", name="adoption_configration_messages", requirements={"contact_id" = "\d+"})
-     * @Template("animalline/adoption/configration/message.twig")
-     */
-    public function adoption_configration_message(Request $request, $contact_id)
-    {
-        $rootMessage = $this->conservationContactsRepository->find($contact_id);
-        if (!$rootMessage) {
-            throw new HttpException\NotFoundHttpException();
-        }
-
-        $description = $request->get('contact_description');
-
-        $conservationContact = new ConservationContacts();
-        $form = $this->createFormBuilder($conservationContact)->getForm();
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()) {
-            $conservationContact->setCustomer($rootMessage->getCustomer())
-                ->setConservation($this->getUser())
-                ->setMessageFrom(AnilineConf::MESSAGE_FROM_CONFIGURATION)
-                ->setPet($rootMessage->getPet())
-                ->setContactType(AnilineConf::CONTACT_TYPE_REPLY)
-                ->setContactDescription($description)
-                ->setParentMessageId($contact_id)
-                ->setSendDate(new DateTime())
-                ->setIsResponse(AnilineConf::RESPONSE_REPLIED)
-                ->setContractStatus(AnilineConf::CONTRACT_STATUS_UNDER_NEGOTIATION)
-                ->setReason(0);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($conservationContact);
-            $entityManager->flush();
-        }
-        $messages = $this->conservationContactsRepository->findBy(
-            ['parent_message_id' => $contact_id],
-            ['send_date' => 'ASC']
-        );
-        return $this->render('animalline/adoption/configration/message.twig', [
-            'rootMessage' => $rootMessage,
-            'messages' => $messages,
-            'form' => $form->createView()
-        ]);
-    }
-
+    
     /**
      * 保護団体用ユーザーページ
      *
