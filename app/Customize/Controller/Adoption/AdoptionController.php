8090ad5c1dffe23f5494c36166511202804a7545
DESKTOP-VROH117\q_sat <kalavinka.asia@gmail.com>
2021-08-17 01:14:29 +0900

    サイドメニュー共通化対応。作業指示用データ作成

--- app/Customize/Controller/Adoption/AdoptionController.php
+++ app/Customize/Controller/Adoption/AdoptionController.php
@@ -143,7 +143,7 @@ class AdoptionController extends AbstractController
     /**
      * お気に入り一覧.
      *
-     * @Route("/adoption/favolite", name="adoption_favolite")
+     * @Route("/adoption/member/favolite", name="adoption_favolite")
      * @Template("animalline/adoption/favolite.twig")
      */
     public function favolite(Request $request)
