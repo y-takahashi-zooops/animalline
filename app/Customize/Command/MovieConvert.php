811a1caf48f936ee8e15323eb16db7e7030714df
quyenhn-miagi <quyenhn@miagi-so.net>
2021-08-19 14:09:48 +0700

    update logic ffmpeg

--- app/Customize/Command/MovieConvert.php
+++ app/Customize/Command/MovieConvert.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace Customize\Command;
 
 use Customize\Entity\BatchSchedule;
+use Customize\Repository\MovieConvertRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  */
@@ -19,16 +22,21 @@ class MovieConvert extends Command
      */
     protected $entityManager;
 
+    protected $movieConvertRepository;
+
     /**
      * @var SymfonyStyle
      */
     protected $io;
 
     public function __construct(
-        EntityManagerInterface $entityManager
-    ) {
+        EntityManagerInterface $entityManager,
+        MovieConvertRepository $movieConvertRepository
+    )
+    {
         parent::__construct();
         $this->entityManager = $entityManager;
+        $this->movieConvertRepository = $movieConvertRepository;
     }
 
     protected function configure()
@@ -43,6 +51,28 @@ class MovieConvert extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $em = $this->entityManager;
+        $movies = $this->movieConvertRepository->findBy(['convert_status' => 0]);
 
+        if (!$movies) {
+            throw new NotFoundHttpException("Cannot find video in queue!");
+        }
+
+        foreach ($movies as $movie) {
+            $cmd = 'ffmpeg -y -i ' . $movie->getSourcePath() . ' -r 30 -vb 340 ' . $movie->getDistPath() . ' -hide_banner -loglevel error 2>&1';
+            $res = exec($cmd, $errors);
+            if ($res) {
+                $movie->setConvertStatus(2);
+                $movie->setErrorReason($res);
+                echo 'Caught exception: ', $res, "\n";
+            } else {
+                $movie->setConvertStatus(1);
+                $movie->setErrorReason(null);
+                echo 'File has been converted!', "\n";
+            }
+            $em->persist($movie);
+            $em->flush();
+        }
+        return 0;
     }
 }
