18756d3634e6041a3b5ce26f8dcb5fe98d87b18b
Kim Lam <lamkd@miagi-so.net>
2021-08-31 17:25:55 +0700

    change solution search conservation

--- app/Customize/Controller/Admin/AdoptionController.php
+++ app/Customize/Controller/Admin/AdoptionController.php
@@ -45,30 +45,10 @@ class AdoptionController extends AbstractController
      */
     public function index(PaginatorInterface $paginator, Request $request)
     {
-        $request = $request->query->all();
-        $criterial = [];
-        if (isset($request['organization_name']) && !empty($request['organization_name'])) {
-            $criterial['organization_name'] = $request['organization_name'];
-        }
-
-        if (isset($request['examination_status'])) {
-            switch ($request['examination_status']) {
-                case 1:
-                    break;
-                case 2:
-                    $criterial['examination_status'] = [1, 2];
-                    break;
-                case 3:
-                    $criterial['examination_status'] = 0;
-                    break;
-            }
-        }
-
-        $order = isset($request['ordering']) ? [$request['ordering'] => 'DESC'] : ['create_date' => 'DESC'];
-        $results = $this->conservationsRepository->findBy($criterial, $order);
+        $results = $this->conservationsRepository->searchConservations($request->query->all());
         $conservations = $paginator->paginate(
             $results,
-            $request['page'] ?? 1,
+            $request->query->getInt('page', 1),
             AnilineConf::ANILINE_NUMBER_ITEM_PER_PAGE
         );
 
