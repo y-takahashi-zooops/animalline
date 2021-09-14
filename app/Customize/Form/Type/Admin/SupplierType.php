a49499195aa2dc0b28263e7bdf2ed8f982bebf7c
hungtm <hungtm@miagi-so.net>
2021-09-14 10:56:42 +0700

    Add Supplier Type

---
+++ app/Customize/Form/Type/Admin/SupplierType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Customize\Form\Type\Admin;
+
+use Customize\Entity\Supplier;
+use Eccube\Common\EccubeConfig;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SupplierType extends AbstractType
+{
+    /**
+     * @var EccubeConfig
+     */
+    protected $eccubeConfig;
+
+    public function __construct(EccubeConfig $eccubeConfig)
+    {
+        $this->eccubeConfig = $eccubeConfig;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('supplier_name', TextType::class, [
+                'attr' => [
+                    'maxlength' => $this->eccubeConfig['eccube_stext_len'],
+                ],
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => $this->eccubeConfig['eccube_stext_len'],
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[^\s ]+$/u',
+                        'message' => 'form_error.not_contain_spaces',
+                    ]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('supplier_code', TextType::class, [
+                'attr' => [
+                    'maxlength' => $this->eccubeConfig['eccube_stext_len'],
+                ],
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => $this->eccubeConfig['eccube_stext_len'],
+                    ]),
+                    new Assert\NotBlank()
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Supplier::class,
+        ]);
+    }
+}
