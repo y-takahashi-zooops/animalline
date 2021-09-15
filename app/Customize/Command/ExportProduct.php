58b0cc9498d08f5b8922e92c4293676ad894f9e7
quyenhn-miagi <quyenhn@miagi-so.net>
2021-09-15 17:05:47 +0700

    fix conflic and add query

--- app/Customize/Command/ExportProduct.php
+++ app/Customize/Command/ExportProduct.php
@@ -2,10 +2,13 @@
 
 namespace Customize\Command;
 
+use Carbon\Carbon;
 use Customize\Config\AnilineConf;
+use Customize\Repository\WmsSyncInfoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Eccube\Repository\ProductRepository;
 use Exception;
+use Eccube\Repository\ProductClassRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -28,14 +31,32 @@ class ExportProduct extends Command
      */
     protected $io;
 
+    /**
+     * @var ProductClassRepository
+     */
+    protected $productClassRepository;
+
+    /**
+     * @var ProductRepository
+     */
     protected $productRepository;
+
+    /**
+     * @var WmsSyncInfoRepository
+     */
+    protected $wmsSyncInfoRepository;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        ProductRepository $productRepository
-    ) {
+        WmsSyncInfoRepository  $wmsSyncInfoRepository,
+        ProductClassRepository $productClassRepository,
+        ProductRepository      $productRepository
+    )
+    {
         parent::__construct();
         $this->entityManager = $entityManager;
+        $this->wmsSyncInfoRepository = $wmsSyncInfoRepository;
+        $this->productClassRepository = $productClassRepository;
         $this->productRepository = $productRepository;
     }
 
@@ -49,26 +70,49 @@ class ExportProduct extends Command
         $this->io = new SymfonyStyle($input, $output);
     }
 
+    /**
+     * @throws Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $newDate = date('Ymd_His');
-        $dir = '/var/tmp/wms/items';
-        $file = "SHNMST_$newDate.csv";
-        $path = "$dir/$file";
+        $dir = 'var/tmp/wms/items';
+
         if (!file_exists($dir)) {
             if (!mkdir($dir, 0777)) throw new Exception('Can not create folder.');
         }
-        if (!$targetFile = fopen($path, "w")) throw new Exception('Can not create file.');
 
-        $data = [
-            [1, 2, 3],
-            [4, 5, 6]
-        ];
-        if ($data) {
-            foreach ($data as $row) {
-                fputcsv($targetFile, $row);
+        $syncDate = $this->wmsSyncInfoRepository->findOneBy(['sync_action' => 1], ['sync_date' => 'ASC'])->getSyncDate();
+        $newDate = Carbon::now();
+
+        $qb = $this->productClassRepository->createQueryBuilder('pc');
+        $qb
+            ->select('productCode')
+            ->leftJoin('pc.Product', 'p')
+            ->add('where', $qb->expr()->between(
+                'p.update_date',
+                ':from',
+                ':to')
+            )
+            ->setParameters(array('from' => $syncDate, 'to' => $newDate))
+            ->orderBy('p.update_date', 'DESC');
+
+        $records = $qb->getQuery()->getArrayResult();
+//        echo (getcwd());
+        $filename = 'SHNMST_' . Carbon::now()->format('Ymd_His') . '.csv';
+        if ($records) {
+            $csvPath = 'var/tmp/wms/items/' . $filename;
+
+            $csvh = fopen($csvPath, 'w+') or die("Can't open file");
+            $d = ','; // this is the default but i like to be explicit
+            $e = '"'; // this is the default but i like to be explicit
+
+            foreach ($records as $record) {
+                fputcsv($csvh, $record, $d, $e);
             }
-            fclose($targetFile);
+
+            fclose($csvh);
+
+            // do something with the file
         }
     }
 }
