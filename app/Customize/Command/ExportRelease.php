3622a40252b6a207af977a870b9f4bba4b20a7f3
hieucv <hieucv@miagi-so.net>
2021-09-17 16:09:45 +0700

    logic csv shipping

--- app/Customize/Command/ExportRelease.php
+++ app/Customize/Command/ExportRelease.php
@@ -96,6 +96,7 @@ class ExportRelease extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $isShipping = false;
         $em = $this->entityManager;
         $fieldSorted = [
             'shippingInstructionNo', 'expectedShippingDate', 'expectedArrivalDate', 'arrivalTime', 'storeCode', 'warehouseCode',
@@ -118,33 +119,7 @@ class ExportRelease extends Command
         $syncInfo = $this->wmsSyncInfoRepository->findOneBy(['sync_action' => 4], ['sync_date' => 'DESC']);
 
         $qb = $this->shippingScheduleRepository->createQueryBuilder('ss');
-        $qb->select(
-            'IDENTITY(ssh.Shipping) as shippingInstructionNo',
-            'ssh.shipping_date_schedule',
-            'ssh.arrival_date_schedule',
-            'ssh.arrival_time_code_schedule',
-            'ssh.arrival_time_code_schedule',
-            'ss.warehouse_code',
-            'ss.item_code_01',
-            'ss.item_code_02',
-            'ss.jan_code',
-            'ss.quantity',
-            'ss.standerd_price',
-            'ss.selling_price',
-            'ssh.customer_name',
-            'ssh.customer_zip',
-            'ssh.customer_address',
-            'ssh.customer_tel',
-            'ssh.total_price',
-            'ssh.discounted_price',
-            'ssh.tax_price',
-            'ssh.postage_price',
-            'ssh.total_weight',
-            'ssh.shipping_units',
-            'ss.item_code_01',
-            'IDENTITY(ssh.Shipping) as slipOutputOrder'
-        )
-            ->innerJoin('ss.ShippingScheduleHeader', 'ssh')
+        $qb->innerJoin('ss.ShippingScheduleHeader', 'ssh')
             ->leftJoin('ssh.Shipping', 's')
             ->where('s.update_date <= :to')
             ->setParameters(['to' => $now]);
@@ -162,12 +137,27 @@ class ExportRelease extends Command
         $arr = array_column($query, 'id');
 
         $filename = 'SHUSJI' . $now->format('Ymd_His') . '.csv';
-
+        $queryShipping = $this->shippingScheduleHeaderRepository->createQueryBuilder('ssh')
+            ->innerJoin('ssh.Shipping', 's')
+            ->select('s.id')
+            ->where('s.update_date <= :to')
+            ->setParameters(['to' => $now]);
+        if ($syncInfo) $qb = $qb->andWhere('s.update_date >= :from')
+            ->setParameter('from', $syncInfo->getSyncDate());
+        $arrId = array_column($queryShipping->getQuery()->getArrayResult(), 'id');
+        $arrDiff = array_diff($arr, $arrId);
+        $wms = new WmsSyncInfo();
         if ($records) {
-            $isShipping = false;
-            $wms = new WmsSyncInfo();
             $wms->setSyncAction(4)
                 ->setSyncDate($now);
+            $wms->setSyncResult(1);
+            $wms->setSyncLog("alert");
+        }
+        if ($arrDiff) {
+            $queryNotInHeaders = $this->shippingRepository->createQueryBuilder('s');
+            $queryNotInHeaders->andWhere('s.id in (:arr)')
+                ->setParameter('arr', $arrDiff);
+            $queryNotInHeaders = $queryNotInHeaders->getQuery()->getArrayResult();
             try {
                 $csvPath = $dir . '/' . $filename;
                 $csvh = fopen($csvPath, 'w+') or die("Can't open file");
@@ -176,95 +166,136 @@ class ExportRelease extends Command
 
                 $result = [];
 
-                foreach ($records as $record) {
-                    if (in_array($record['shippingInstructionNo'], $arr)) {
-                        $isShipping = true;
-                        $wms->setSyncLog("alert");
-                        continue;
-                    }
-                    $record['storeCode'] = null;
-                    $record['saleCategory'] = '0';
-                    $record['multiplicationRate'] = null;
-                    $record['slipType'] = '0';
-                    $record['sizeCode'] = '1';
-                    $record['shippingCompanyCode'] = '000002';
-                    $record['remarks'] = null;
-                    $record['detailsRemarks'] = null;
-                    $record['businessUnitName'] = null;
-                    $record['areaName'] = null;
-                    $record['destinationCode'] = null;
-                    $record['destinationCode'] = null;
-                    $record['areaCode'] = null;
-                    $record['BBDATE'] = null;
-                    $record['deliveryDestinationClassification'] = '1';
-                    $record['coupon'] = null;
-                    $record['paymentMethodClassification'] = '0';
-                    $record['remark_2'] = null;
-                    $record['remark_3'] = null;
-                    $record['salesDestinationClassification'] = '01';
-                    $record['commission'] = null;
-                    $record['handlingFlightTypes'] = '000';
-                    $record['destinationClassification'] = '1';
-                    $sorted = [];
-                    foreach ($fieldSorted as $value) {
-                        array_push($sorted, $record[$value]);
-                    }
-                    array_push($result, $sorted);
-                }
-                foreach ($records as $record) {
-                    $shipping = $this->shippingRepository->find($id=$record['shippingInstructionNo']);
+                foreach ($queryNotInHeaders as $queryNotInHeader) {
+                    $shipping = $this->shippingRepository->find($queryNotInHeader['id']);
                     $order = $shipping->getOrder();
-                    $orderItem = $this->orderItemRepository->find($record['shippingInstructionNo']);
+                    $orderItem = $this->orderItemRepository->findOneBy(['Shipping' => $shipping]);
+                    if ($orderItem) {
+                        $shippingScheduleHeader = new ShippingScheduleHeader();
+                        $shippingScheduleHeader
+                            ->setShipping($shipping)
+                            ->setShippingDateSchedule($shipping->getShippingDate())
+                            ->setArrivalDateSchedule($shipping->getShippingDeliveryDate())
+                            ->setArrivalTimeCodeSchedule($shipping->getShippingDeliveryTime())
+                            ->setCustomerName($shipping->getName01() . $shipping->getName02())
+                            ->setCustomerZip($shipping->getPostalCode())
+                            ->setCustomerAddress($shipping->getAddr01() . $shipping->getAddr02())
+                            ->setCustomerTel($shipping->getPhoneNumber())
+                            ->setTotalPrice($order->getSubTotal())
+                            ->setDiscountedPrice($order->getDiscount())
+                            ->setTaxPrice($order->getTax())
+                            ->setPostagePrice($order->getDeliveryFeeTotal())
+                            ->setTotalWeight($orderItem->getProductClass()
+                                ->getProduct()->getItemWeight()
+                            )
+                            ->setShippingUnits(round(
+                                (float)$orderItem->getProductClass()->getProduct()->getItemWeight() / 20))
+                            ->setWmsSendDate($now)
+                            ->setIsCancel(0);
+                        $em->persist($shippingScheduleHeader);
+                        $em->flush();
 
-                    $shippingScheduleHeader = new ShippingScheduleHeader();
-                    $shippingScheduleHeader->setShippingDateSchedule($now)
-                                           ->setShipping($shipping)
-                                           ->setShippingDateSchedule($shipping->getShippingDate())
-                                           ->setArrivalDateSchedule($shipping->getShippingDeliveryDate())
-                                           ->setArrivalTimeCodeSchedule($shipping->getShippingDeliveryTime())
-                                           ->setCustomerName($shipping->getName01() . $shipping->getName02())
-                                           ->setCustomerZip($shipping->getPostalCode())
-                                           ->setCustomerAddress($shipping->getAddr01() . $shipping->getAddr02())
-                                           ->setCustomerTel($shipping->getPhoneNumber())
-                                           ->setTotalPrice($order->getSubTotal())
-                                           ->setDiscountedPrice($order->getDiscount())
-                                           ->setTaxPrice($order->getTax())
-                                           ->setPostagePrice($order->getDeliveryFeeTotal())
-                                           ->setTotalWeight($orderItem->getProductClass()
-                                                                      ->getProduct()->getItemWeight()
-                                                           )
-                                           ->setShippingUnits(round(
-                                               (float)$orderItem->getProductClass()->getProduct()->getItemWeight() / 20))
-                                           ->setWmsSendDate($now)
-                                           ->setIsCancel(0);
+                        $shippingSchedule = new ShippingSchedule();
 
-                    $shippingSchedule = new ShippingSchedule();
+                        $shippingSchedule->setWarehouseCode('00000')
+                            ->setItemCode01($orderItem->getProductClass()->getCode())
+                            ->setJanCode($orderItem->getProductClass()->getCode())
+                            ->setQuantity($orderItem->getQuantity())
+                            ->setStanderdPrice($orderItem->getPrice())
+                            ->setSellingPrice($orderItem->getPrice())
+                            ->setShippingScheduleHeader($shippingScheduleHeader)
+                            ->setOrderDetail($orderItem)
+                            ->setProductClass($orderItem->getProductClass());
 
-                    $shippingSchedule->setWarehouseCode($record['warehouse_code'])
-                                     ->setItemCode01($orderItem->getProductClass()->getCode())
-                                     ->setJanCode($orderItem->getProductClass()->getCode())
-                                     ->setQuantity($orderItem->getQuantity())
-                                     ->setStanderdPrice($orderItem->getPrice())
-                                     ->setSellingPrice($orderItem->getPrice())
-                                     ->setShippingScheduleHeader($shippingScheduleHeader)
-                                     ->setOrderDetail($orderItem)
-                                     ->setProductClass($orderItem->getProductClass());
+                        $em->persist($shippingSchedule);
+                        $em->flush();
+                        $sorted = [];
+                        $queryCsv = $this->shippingScheduleRepository->createQueryBuilder('ss');
+                        $queryCsv->select(
+                            'IDENTITY(ssh.Shipping) as shippingInstructionNo',
+                            'ssh.shipping_date_schedule',
+                            'ssh.arrival_date_schedule',
+                            'ssh.arrival_time_code_schedule',
+                            'ssh.arrival_time_code_schedule',
+                            'ss.warehouse_code',
+                            'ss.item_code_01',
+                            'ss.item_code_02',
+                            'ss.jan_code',
+                            'ss.quantity',
+                            'ss.standerd_price',
+                            'ss.selling_price',
+                            'ssh.customer_name',
+                            'ssh.customer_zip',
+                            'ssh.customer_address',
+                            'ssh.customer_tel',
+                            'ssh.total_price',
+                            'ssh.discounted_price',
+                            'ssh.tax_price',
+                            'ssh.postage_price',
+                            'ssh.total_weight',
+                            'ssh.shipping_units',
+                            'ss.item_code_01',
+                            'IDENTITY(ssh.Shipping) as slipOutputOrder'
+                        )
+                            ->innerJoin('ss.ShippingScheduleHeader', 'ssh')
+                            ->leftJoin('ssh.Shipping', 's')
+                            ->where('ssh.Shipping = :shipping')
+                            ->andWhere('s.update_date <= :to')
+                            ->setParameters(['to' => $now, 'shipping' => $shipping]);
+                        if ($syncInfo) $queryCsv = $queryCsv->andWhere('s.update_date >= :from')
+                            ->setParameter('from', $syncInfo->getSyncDate());
+                        $queryCsv = $queryCsv->orderBy('s.update_date', 'DESC');
+                        $recordCsvs = $queryCsv->getQuery()->getArrayResult();
+                        foreach ($recordCsvs as $recordCsv) {
+                            $recordCsv['storeCode'] = null;
+                            $recordCsv['saleCategory'] = '0';
+                            $recordCsv['multiplicationRate'] = null;
+                            $recordCsv['slipType'] = '0';
+                            $recordCsv['sizeCode'] = '1';
+                            $recordCsv['shippingCompanyCode'] = '000002';
+                            $recordCsv['remarks'] = null;
+                            $recordCsv['detailsRemarks'] = null;
+                            $recordCsv['businessUnitName'] = null;
+                            $recordCsv['areaName'] = null;
+                            $recordCsv['destinationCode'] = null;
+                            $recordCsv['destinationCode'] = null;
+                            $recordCsv['areaCode'] = null;
+                            $recordCsv['BBDATE'] = null;
+                            $recordCsv['deliveryDestinationClassification'] = '1';
+                            $recordCsv['coupon'] = null;
+                            $recordCsv['paymentMethodClassification'] = '0';
+                            $recordCsv['remark_2'] = null;
+                            $recordCsv['remark_3'] = null;
+                            $recordCsv['salesDestinationClassification'] = '01';
+                            $recordCsv['commission'] = null;
+                            $recordCsv['handlingFlightTypes'] = '000';
+                            $recordCsv['destinationClassification'] = '1';
+                            $sorted = [];
+                            foreach ($fieldSorted as $value) {
+                                array_push($sorted, $recordCsv[$value]);
+                            }
+                            array_push($result, $sorted);
+                        }
+                        foreach ($result as $item) {
+                            fputcsv($csvh, $item, $d, $e);
+                        }
+                        fclose($csvh);
+                        echo 'Export succeeded.';
+                    }
+                    echo 'Export succeeded.';
                 }
-                foreach ($result as $item) {
-                    fputcsv($csvh, $item, $d, $e);
-                }
-                fclose($csvh);
-                $wms->setSyncResult($isShipping ? 2 : 1);
-                echo 'Export succeeded.';
+                $wms->setSyncAction(4)
+                    ->setSyncDate($now);
+                $wms->setSyncResult(1);
+                $wms->setSyncLog("alert");
             } catch (Exception $e) {
+                $wms = new WmsSyncInfo();
                 $wms->setSyncResult(3)
-                ->setSyncLog($e->getMessage());
-                echo 'Export failed.';
+                    ->setSyncLog($e->getMessage());
+                echo $e->getMessage();
             }
-            $em->persist($wms);
-            $em->persist($shippingSchedule);
-            $em->persist($shippingScheduleHeader);
-            $em->flush();
         }
+        $em->persist($wms);
+        $em->flush();
     }
 }
