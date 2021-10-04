5ebfe8165d0ef4c3b82390c79e6090b25278b1a1
quyenhn-miagi <quyenhn@miagi-so.net>
2021-10-04 21:13:30 +0700

    complete update/del product set

--- app/Customize/Controller/Admin/Product/ProductSetController.php
+++ app/Customize/Controller/Admin/Product/ProductSetController.php
@@ -13,12 +13,19 @@
 
 namespace Customize\Controller\Admin\Product;
 
+use Customize\Entity\ProductSet;
+use Customize\Form\Type\Admin\ProductSetType;
+use Customize\Repository\ProductSetRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Eccube\Controller\AbstractController;
+use Eccube\Entity\OrderItem;
 use Eccube\Form\Type\Admin\SearchProductType;
+use Eccube\Repository\Master\OrderItemTypeRepository;
 use Eccube\Repository\ProductClassRepository;
 use Eccube\Repository\ProductRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductSetController extends AbstractController
@@ -34,17 +41,32 @@ class ProductSetController extends AbstractController
     protected $productRepository;
 
     /**
+     * @var ProductSetRepository
+     */
+    protected $productSetRepository;
+
+    /**
+     * @var OrderItemTypeRepository
+     */
+    protected $orderItemTypeRepository;
+
+    /**
      * ProductController constructor.
      *
      * @param ProductClassRepository $productClassRepository
      * @param ProductRepository $productRepository
+     * @param ProductSetRepository $productSetRepository
      */
     public function __construct(
-        ProductClassRepository          $productClassRepository,
-        ProductRepository               $productRepository
+        ProductClassRepository  $productClassRepository,
+        ProductRepository       $productRepository,
+        ProductSetRepository    $productSetRepository,
+        OrderItemTypeRepository $orderItemTypeRepository
     ) {
         $this->productClassRepository = $productClassRepository;
         $this->productRepository = $productRepository;
+        $this->productSetRepository = $productSetRepository;
+        $this->orderItemTypeRepository = $orderItemTypeRepository;
     }
 
     /**
@@ -57,17 +79,90 @@ class ProductSetController extends AbstractController
     {
         $Product = $this->productRepository->find($id);
         $ProductClass = $this->productClassRepository->findOneBy(['Product' => $Product]);
+        if (!$Product || !$ProductClass) {
+            throw new NotFoundHttpException();
+        }
+        $OriginSets = new ArrayCollection();
+        foreach ($this->productSetRepository->findBy(['ParentProduct' => $Product]) as $set) {
+            $item = new OrderItem;
+            $item->setId($set->getId());
+            $item->setOrderItemType($this->orderItemTypeRepository->find(1));
+            $item->setQuantity($set->getSetUnit());
+            $item->setProduct($set->getProduct());
+            $item->setPrice($set->getProductClassId()->getPrice02());
+            $item->setProductClass($set->getProductClassId());
+            $item->setProductName($set->getProduct()->getName());
+            $item->setProductCode($set->getProductClassId()->getCode());
+            if ($set->getProductClassId()->getClassCategory1()) {
+                $item->setClassName1('フレーバー');
+                $item->setClassCategoryName1($set->getProductClassId()->getClassCategory1()->getName());
+            }
+            if ($set->getProductClassId()->getClassCategory2()) {
+                $item->setClassName2('サイズ');
+                $item->setClassCategoryName2($set->getProductClassId()->getClassCategory2()->getName());
+            }
+            $OriginSets->add($item);
+        }
+        $Product->setProductSet();
+        foreach ($OriginSets as $key => $item) {
+            $Product->addProductSet($item);
+        }
+        $builder = $this->formFactory->createBuilder(ProductSetType::class, $Product);
+        $form = $builder->getForm();
+        $form->handleRequest($request);
 
-        // if ($form->isSubmitted() && $form->isValid()) {
-        // }
+        if ($form->isSubmitted() && $form['ProductSet']->isValid()) {
+            $items = $form['ProductSet']->getData();
+            switch ($request->get('mode')) {
+                case 'register':
+                    if ($form->isValid()) {
+                        $Product->setProductSet(); // clear temp orderitem data
+                        $this->entityManager->persist($Product);
 
-        // 商品検索フォーム
-        $searchProductModalForm = $this->createForm(SearchProductType::class);
+                        $idDb = [];
+                        $idReq = [];
+                        foreach ($this->productSetRepository->findBy(['ParentProduct' => $Product]) as $item) {
+                            array_push($idDb, $item->getId());
+                        }
+                        foreach ($items as $key => $item) {
+                            array_push($idReq, $item['id']);
+                            if ($item['id']) {
+                                $ProductSet = $this->productSetRepository->find($item['id']);
+                                $ProductSet->setSetUnit($item->getQuantity());
+                            } else {
+                                $ProductSet = (new ProductSet())
+                                    ->setSetUnit($item->getQuantity())
+                                    ->setParentProduct($Product)
+                                    ->setParentProductClass($ProductClass)
+                                    ->setProduct($item->getProduct())
+                                    ->setProductClassId($item->getProductClass());
+                            }
+                            $this->entityManager->persist($ProductSet);
+                        }
+                        foreach ($idDb as $item) {
+                            if (!in_array($item, $idReq)) {
+                                $objDel = $this->productSetRepository->find($item);
+                                $this->entityManager->remove($objDel);
+                            }
+                        }
+                        $this->entityManager->flush();
 
+                        $this->addSuccess('admin.common.save_complete', 'admin');
+                        return $this->redirectToRoute('admin_product');
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        $builder = $this->formFactory->createBuilder(SearchProductType::class);
+        $searchProductModalForm = $builder->getForm();
         return [
+            'form' => $form->createView(),
+            'searchProductModalForm' => $searchProductModalForm->createView(),
             'Product' => $Product,
             'ProductClass' => $ProductClass,
-            'searchProductModalForm' => $searchProductModalForm->createView()
+            'id' => $id
         ];
     }
 }
