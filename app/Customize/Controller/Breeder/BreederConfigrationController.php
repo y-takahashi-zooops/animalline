b8cb9197e2825c6621871a569866edf95020504f
hieucv <hieucv@miagi-so.net>
2021-10-04 17:23:19 +0700

    fix bug upload image

--- app/Customize/Controller/Breeder/BreederConfigrationController.php
+++ app/Customize/Controller/Breeder/BreederConfigrationController.php
@@ -261,25 +261,6 @@ class BreederConfigrationController extends AbstractController
 //        return new JsonResponse($data);
 //    }
 
-//    /**
-//     * Upload image
-//     *
-//     * @Route("/breeder/configration/pets/upload", name="breeder_pets_upload_crop_image", methods={"POST"}, options={"expose"=true})
-//     */
-//    public function upload(Request $request)
-//    {
-//        if (!file_exists(AnilineConf::ANILINE_IMAGE_URL_BASE . '/tmp/')) {
-//            mkdir(AnilineConf::ANILINE_IMAGE_URL_BASE . '/tmp/', 0777, 'R');
-//        }
-//        $folderPath = AnilineConf::ANILINE_IMAGE_URL_BASE . '/tmp/';
-//        $image_parts = explode(";base64,", $_POST['image']);
-//        $image_type_aux = explode("image/", $image_parts[0]);
-//        $image_type = $image_type_aux[1];
-//        $image_base64 = base64_decode($image_parts[1]);
-//        $file = $folderPath . uniqid() . '.' . $image_type;
-//        file_put_contents($file, $image_base64);
-//        return new JsonResponse($file);
-//    }
 //
 //    /**
 //     * Breeder base information
--- app/Customize/Controller/Breeder/BreederHouseController.php
+++ app/Customize/Controller/Breeder/BreederHouseController.php
@@ -95,64 +95,6 @@ class BreederHouseController extends AbstractController
     }
 
     /**
-     * 基本情報編集画面
-     *
-     * @Route("/breeder/member/baseinfo", name="breeder_baseinfo")
-     * @Template("/animalline/breeder/member/base_info.twig")
-     */
-    public function base_info(Request $request, BreedersRepository $breedersRepository)
-    {
-        //リダイレクト先設定
-        $return_path = $request->get('return_path');
-        if ($return_path == "") {
-            $return_path = "breeder_examination";
-        }
-
-        $user = $this->getUser();
-
-        $breederData = $breedersRepository->find($user);
-        if (!$breederData) {
-            $breederData = new Breeders;
-            $breederData->setId($user->getId());
-        }
-        $builder = $this->formFactory->createBuilder(BreedersType::class, $breederData);
-
-        $form = $builder->getForm();
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $thumbnail_path = $request->get('thumbnail_path') ? $request->get('thumbnail_path') : $breederData->getThumbnailPath();
-            $license_thumbnail_path = $request->get('license_thumbnail_path') ? $request->get('license_thumbnail_path') : $breederData->getLicenseThumbnailPath();
-
-            $breederData->setBreederPref($breederData->getPrefBreeder())
-                ->setLicensePref($breederData->getPrefLicense())
-                ->setThumbnailPath($thumbnail_path)
-                ->setLicenseThumbnailPath($license_thumbnail_path);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($breederData);
-            $entityManager->flush();
-            return $this->redirectToRoute($return_path);
-        } elseif (!$form->isSubmitted()) {
-
-            // Customer情報から初期情報をセット
-            $Customer = $this->customerRepository->find($user);
-            $form->get('breeder_name')->setData($Customer->getname01() . '　' . $Customer->getname02());
-            $form->get('breeder_kana')->setData($Customer->getkana01() . '　' . $Customer->getkana02());
-            $form->get('breeder_zip')->setData($Customer->getPostalCode());
-            $form->get('addr')->get('PrefBreeder')->setData($Customer->getPref());
-            $form->get('addr')->get('breeder_city')->setData($Customer->getAddr01());
-            $form->get('addr')->get('breeder_address')->setData($Customer->getAddr02());
-            $form->get('breeder_tel')->setData($Customer->getPhoneNumber());
-        }
-
-        return [
-            'return_path' => $return_path,
-            'breederData' => $breederData,
-            'form' => $form->createView()
-        ];
-    }
-
-    /**
      * 犬舎・猫舎情報編集画面
      *
      * @Route("/breeder/member/house_info/{pet_type}", name="breeder_house_info")
