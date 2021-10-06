217eb59754aa9e30c4425d4be351ae848c2de996
hungtm <hungtm@miagi-so.net>
2021-10-06 17:09:51 +0700

    Fix sort

--- app/Customize/Service/VeqtaQueryService.php
+++ app/Customize/Service/VeqtaQueryService.php
@@ -56,7 +56,7 @@ class VeqtaQueryService
             ->where('dna.check_status = 3')
             ->andWhere('dna.site_type = :site_type')
             ->setParameters(['site_type' => AnilineConf::ANILINE_SITE_TYPE_ADOPTION])
-            ->select('dna.id, dna.site_type, b.breeds_name, cp.pet_birthday, dna.check_status, dna.kit_pet_register_date');
+            ->select('dna.id as dna_id, dna.site_type, b.breeds_name, cp.pet_birthday, dna.check_status, dna.kit_pet_register_date');
         $resultConservation = $queryConservation->getQuery()->getArrayResult();
 
         $queryBreeder = $this->dnaCheckStatusRepository->createQueryBuilder('dna')
@@ -65,12 +65,19 @@ class VeqtaQueryService
             ->where('dna.check_status = 3')
             ->andWhere('dna.site_type = :site_type')
             ->setParameters(['site_type' => AnilineConf::ANILINE_SITE_TYPE_BREEDER])
-            ->select('dna.id, dna.site_type, b.breeds_name, bp.pet_birthday, dna.check_status, dna.kit_pet_register_date');
-        $resultBreeder = $queryBreeder->orderBy('dna.update_date', 'DESC')
-                                      ->getQuery()
-                                      ->getArrayResult();
+            ->select('dna.id as dna_id, dna.site_type, b.breeds_name, bp.pet_birthday, dna.check_status, dna.kit_pet_register_date');
+        $resultBreeder = $queryBreeder->getQuery()->getArrayResult();
 
         $totalResult = array_merge($resultBreeder, $resultConservation);
+
+        // order by update_date > dna_id desc
+        usort(
+            $totalResult,
+            fn ($x, $y) =>
+            [$y['update_date']->getTimestamp(), $y['dna_id']]
+                <=>
+                [$x['update_date']->getTimestamp(), $x['dna_id']]
+        );
 
         return $totalResult;
     }
