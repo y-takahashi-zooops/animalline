404e688df796db4b4c44f18c44d3e3d4074c20ba
quyenhn-miagi <quyenhn@miagi-so.net>
2021-10-07 18:06:01 +0700

    remove crop image in baseinfo breeder

--- html/template/default/assets/js/baseinfo.js
+++ html/template/default/assets/js/baseinfo.js
@@ -2,72 +2,25 @@ $("form[name=update-baseinfo-form]").submit(function (e) {
     e.preventDefault();
     $('#modal').modal('show');
 });
-
 $("#confirm-yes").click(function (e) {
     document.getElementsByName('update-baseinfo-form')[0].submit();
 });
 
-var bs_modal = $('#modal-crop');
-var image = document.getElementById('image');
-var cropper, reader, file, size;
 var breederImgFlg = false;
-
-
 $("body").on("change", "#breeders_thumbnail_path", function (e) {
     breederImgFlg = true;
 });
 $("body").on("change", "#breeders_license_thumbnail_path", function (e) {
     breederImgFlg = false;
 });
-
 $("body").on("change", ".thumbnail_path", function (e) {
-    e.preventDefault();
-    var files = e.target.files;
-    var done = function (url) {
-        image.src = url;
-        bs_modal.modal('show');
-    };
-
-    if (files && files.length > 0) {
-        file = files[0];
-
-        if (URL) {
-            done(URL.createObjectURL(file));
-        } else if (FileReader) {
-            reader = new FileReader();
-            reader.onload = function (e) {
-                done(reader.result);
-            };
-            reader.readAsDataURL(file);
-        }
-    }
+    readURL(this)
 });
 
-bs_modal.on('shown.bs.modal', function () {
-    cropper = new Cropper(image, {
-        aspectRatio: 1,
-        viewMode: 2,
-        preview: '.preview',
-        dragMode: 'none',
-    });
-}).on('hidden.bs.modal', function () {
-    cropper.destroy();
-    cropper = null;
-    $("#breeders_thumbnail_path").val('');
-});
-
-$("#crop").click(function () {
-    size = cropper.imageData.naturalHeight < cropper.imageData.naturalWidth ? cropper.imageData.naturalHeight : cropper.imageData.naturalWidth;
-    size = size < 1000 ? size : 1000;
-    canvas = cropper.getCroppedCanvas({
-        width: size,
-        height: size,
-    });
-
-    canvas.toBlob(function (blob) {
-        url = URL.createObjectURL(blob);
-        var reader = new FileReader();
-        reader.readAsDataURL(blob);
+function readURL(input) {
+    if (input.files && input.files[0] && input.files[0].size < 5000000) {
+        let reader = new FileReader();
+        reader.readAsDataURL(input.files[0]);
         reader.onloadend = function () {
             var base64data = reader.result;
             $.ajax({
@@ -76,9 +29,7 @@ $("#crop").click(function () {
                 url: "/breeder/configration/pets/upload",
                 data: {image: base64data},
                 success: function (data) {
-                    bs_modal.modal('hide');
-                    // $('#img_thumbnail').attr('src', '/' + data).removeClass('hidden')
-                    if( breederImgFlg == true ){
+                    if (breederImgFlg === true) {
                         $('#img_thumbnail').attr('src', '/' + data).removeClass('hidden')
                         $('#breeder_src_path').val('/' + data);
                     } else {
@@ -88,5 +39,7 @@ $("#crop").click(function () {
                 }
             });
         };
-    });
-});
+    } else {
+        alert('File too large!')
+    }
+}
