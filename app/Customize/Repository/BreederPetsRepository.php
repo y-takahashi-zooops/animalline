fa137d41e9e6206a14e47f143fd9a2939c1f62c7
hungtm <hungtm@miagi-so.net>
2021-10-18 14:15:58 +0700

    miagi_admin_breeder_pet_list: Add search + list + sort breeder pet

--- app/Customize/Repository/BreederPetsRepository.php
+++ app/Customize/Repository/BreederPetsRepository.php
@@ -2,9 +2,11 @@
 
 namespace Customize\Repository;
 
+use Customize\Config\AnilineConf;
 use Customize\Entity\BreederPets;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Eccube\Util\StringUtil;
 
 /**
  * @method BreederPets|null find($id, $lockMode = null, $lockVersion = null)
@@ -67,5 +69,57 @@ class BreederPetsRepository extends ServiceEntityRepository
             ->setParameter('id', $entity->getId())
             ->getQuery()
             ->execute();
+    }
+
+    /**
+     * Search breederPets
+     *
+     * @param array $order
+     * @return array
+     */
+    public function filterBreederPetsAdmin(array $criteria, array $order): array
+    {
+        $qb = $this->createQueryBuilder('bp');
+        if (!empty($criteria['pet_kind']) && StringUtil::isNotBlank($criteria['pet_kind'])) {
+            $qb
+                ->andWhere($qb->expr()->in('bp.pet_kind', ':pet_kind'))
+                ->setParameter('pet_kind', $criteria['pet_kind']);
+        }
+        if (!empty($criteria['breed_type']) && StringUtil::isNotBlank($criteria['breed_type'])) {
+            $qb
+                ->andWhere($qb->expr()->in('bp.BreedsType', ':BreedsType'))
+                ->setParameter('BreedsType', $criteria['breed_type']);
+        }
+        if (!empty($criteria['public_status']) && StringUtil::isNotBlank($criteria['public_status'])) {
+            if ($criteria['public_status'] == 1) {
+                $qb
+                    ->andWhere($qb->expr()->in('bp.is_active', ':is_active'))
+                    ->setParameter('is_active', AnilineConf::ANILINE_IS_ACTIVE_PRIVATE);
+            }
+            if ($criteria['public_status'] == 2) {
+                $qb
+                    ->andWhere($qb->expr()->in('bp.is_active', ':is_active'))
+                    ->setParameter('is_active', AnilineConf::ANILINE_IS_ACTIVE_PUBLIC);
+            }
+            if ($criteria['public_status'] == 3) {
+                $qb
+                    ->andWhere($qb->expr()->in('bp.dna_check_result', ':dna_check_result'))
+                    ->setParameter('dna_check_result', AnilineConf::DNA_CHECK_RESULT_CHECKING);
+            }
+            if ($criteria['public_status'] == 4) {
+                $qb
+                    ->andWhere($qb->expr()->in('bp.dna_check_result', ':dna_check_result'))
+                    ->setParameter('dna_check_result', AnilineConf::DNA_CHECK_RESULT_CHECK_OK);
+            }
+            if ($criteria['public_status'] == 5) {
+                $qb
+                    ->andWhere($qb->expr()->in('bp.dna_check_result', ':dna_check_result'))
+                    ->setParameter('dna_check_result', AnilineConf::DNA_CHECK_RESULT_CHECK_NG);
+            }
+        }
+        return $qb->orderBy('bp.' . $order['field'], $order['direction'])
+            ->getQuery()
+            ->getResult();
+
     }
 }
