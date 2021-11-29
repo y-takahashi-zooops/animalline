6b2492fd28835b48ffe950163fa768f78ad71b11
hieucv <hieucv@miagi-so.net>
2021-11-29 17:46:22 +0700

    small fix validate

--- src/Eccube/Form/Type/Front/EntryType.php
+++ src/Eccube/Form/Type/Front/EntryType.php
@@ -160,7 +160,7 @@ class EntryType extends AbstractType
         if (!$event->getForm()->getConfig()->getOptions()['password']) {
             $form['passwordErrors']->addError(new FormError('入力されていません。'));
         }
-        if (strlen($event->getForm()->getConfig()->getOptions()['password']) < 32 && strlen($event->getForm()->getConfig()->getOptions()['password'])) {
+        if (strlen($event->getForm()->getConfig()->getOptions()['password']) > 32 || strlen($event->getForm()->getConfig()->getOptions()['password']) < 8) {
             $form['passwordErrors']->addError(new FormError('半角英数記号8〜32文字。'));
         }
     }
