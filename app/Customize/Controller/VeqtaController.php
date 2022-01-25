e35cda3103f3c33e609a00d791575a6213bb7a03
hungtm <hungtm@miagi-so.net>
2022-01-25 11:29:19 +0700

    Fix bug

--- app/Customize/Controller/VeqtaController.php
+++ app/Customize/Controller/VeqtaController.php
@@ -235,7 +235,7 @@ class VeqtaController extends AbstractController
         $countCheckKind = 0;
         if ($dnaCheckKinds) {
             foreach ($dnaCheckKinds as $dnaCheckKind) {
-                $countCheckKind += $dnaCheckKind->getCheckKind();
+                $countCheckKind += $dnaCheckKind->getCheckKind() ?? 0;
             }
         }
 
