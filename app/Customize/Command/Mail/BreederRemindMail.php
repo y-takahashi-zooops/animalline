b08e2a759b94a9eda326fc357585bbdc28356b4e
hoivv <hoivv@miagi-so.net>
2022-01-27 17:45:50 +0700

    rename cmd

--- app/Customize/Command/Mail/BreederRemindMail.php
+++ app/Customize/Command/Mail/BreederRemindMail.php
@@ -16,7 +16,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class ExportBenefitShippingSchedule extends Command
+class BreederRemindMail extends Command
 {
     protected static $defaultName = 'eccube:customize:breeder-remind-mail';
 
