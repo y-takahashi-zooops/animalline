aa7241e392696be378e194f45be05fe04694be5f
zooops-rsato <ryo.sato@zooops-japan.co.jp>
2022-12-07 11:54:49 +0900

    メール送信対応

--- app/Customize/Service/MailService.php
+++ app/Customize/Service/MailService.php
@@ -1653,12 +1653,12 @@ class MailService
         ]);
 
         $message = (new \Swift_Message())
-            ->setSubject('［アニマルライン］キャッシュバックキャンペーンのお知らせ')
+            ->setSubject('【Animalline】年末年始営業日のご案内')
             ->setFrom([$this->BaseInfo->getEmail01() => $this->BaseInfo->getShopName()])
             ->setTo([$Customer->getEmail()])
             ->setReplyTo($this->BaseInfo->getEmail03())
-            ->setReturnPath($this->BaseInfo->getEmail04())
-            ->attach(\Swift_Attachment::fromPath('/var/www/animalline/var/campaign20221125.pdf')->setFilename('12月配布チラシ.pdf')->setContentType('application/pdf'));
+            ->setReturnPath($this->BaseInfo->getEmail04());
+            //->attach(\Swift_Attachment::fromPath('/var/www/animalline/var/campaign20221125.pdf')->setFilename('12月配布チラシ.pdf')->setContentType('application/pdf'));
             
         $message->setBody($body);
 
