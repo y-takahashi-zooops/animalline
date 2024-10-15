d896070ebcae6703eadbcbe9526d9fcb8a9cdfd7
a.wada <a.wada@zooops-japan.co.jp>
2024-10-15 13:47:24 +0900

    【テスト】var_dumpの削除

--- app/Customize/Controller/Breeder/BreederMemberController.php
+++ app/Customize/Controller/Breeder/BreederMemberController.php
@@ -190,7 +190,7 @@ class BreederMemberController extends AbstractController
      */
     public function breeder_mypage(Request $request)
     {
-        $user = $this->getUser();var_dump($user);
+        $user = $this->getUser();
         $breeder = $this->breedersRepository->find($user);
         $canBenefits = false;
 
