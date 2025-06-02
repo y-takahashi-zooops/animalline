b657d495a535e60283cb77cc595d089ec31e955b
y.honmyo <y.honmyo@zooops-japan.co.jp>
2025-06-02 17:33:31 +0900

    管理画面のルート変更

--- src/Eccube/Security/Http/Authentication/EccubeAuthenticationSuccessHandler.php
+++ src/Eccube/Security/Http/Authentication/EccubeAuthenticationSuccessHandler.php
@@ -32,7 +32,7 @@ class EccubeAuthenticationSuccessHandler extends DefaultAuthenticationSuccessHan
         if ($response instanceof Response) {
             $targetUrl = $response->headers->get('Location');
             if ($targetUrl && preg_match('/^https?:\/\//i', $targetUrl)) {
-                $response->headers->set('Location', $request->getUriForPath('/'));
+                $response->headers->set('Location', $request->getUriForPath('/admin_homepage'));
             }
         }
         return $response;
