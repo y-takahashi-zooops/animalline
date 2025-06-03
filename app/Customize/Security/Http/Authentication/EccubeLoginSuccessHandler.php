20d2338a55e383657cf14b17bf5e4c3c239eea18
y.honmyo <y.honmyo@zooops-japan.co.jp>
2025-06-03 17:38:29 +0900

    引数の順番の変更

--- app/Customize/Security/Http/Authentication/EccubeLoginSuccessHandler.php
+++ app/Customize/Security/Http/Authentication/EccubeLoginSuccessHandler.php
@@ -12,7 +12,7 @@ use Psr\Log\LoggerInterface;
 class EccubeLoginSuccessHandler extends DefaultAuthenticationSuccessHandler
 {
     protected $logger;
-    public function __construct(HttpUtils $httpUtils, array $options = [], LoggerInterface $logger)
+    public function __construct(HttpUtils $httpUtils, LoggerInterface $logger, array $options = [], )
     {
         parent::__construct($httpUtils, $options);
         $this->logger = $logger;
