4e45f08df65ecf40818b0bcb6f851d2b44a3cb40
y.honmyo <y.honmyo@zooops-japan.co.jp>
2025-06-03 18:39:01 +0900

    targetPathの設定

--- app/Customize/Security/Http/Authentication/EccubeLoginSuccessHandler.php
+++ app/Customize/Security/Http/Authentication/EccubeLoginSuccessHandler.php
@@ -25,7 +25,7 @@ class EccubeLoginSuccessHandler extends DefaultAuthenticationSuccessHandler
         file_put_contents('/tmp/login_success.log', date('Y-m-d H:i:s') . " - Login success handler called\n", FILE_APPEND);
         $this->logger->info('EccubeLoginSuccessHandler: ログイン成功');
 
-        // $targetPath = $this->defaultOptions['default_target_path'] ?? '/';
+        $targetPath = $this->defaultOptions['default_target_path'] ?? '/';
         $this->logger->info('Redirecting to: ' . $targetPath);
 
         return $this->httpUtils->createRedirectResponse($request, '/admin/');
