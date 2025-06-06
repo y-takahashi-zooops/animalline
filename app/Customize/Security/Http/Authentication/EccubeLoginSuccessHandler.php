154eb3638f34e7ef6fe5d2d3d419f5b55462fda0
y.honmyo <y.honmyo@zooops-japan.co.jp>
2025-06-06 16:10:52 +0900

    SuccessHandlerのコメント削除とPreUpdateEventArgsの追加

--- app/Customize/Security/Http/Authentication/EccubeLoginSuccessHandler.php
+++ app/Customize/Security/Http/Authentication/EccubeLoginSuccessHandler.php
@@ -8,33 +8,19 @@ use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessH
 use Symfony\Component\Security\Http\HttpUtils;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-// use Psr\Log\LoggerInterface;
-
 class EccubeLoginSuccessHandler extends DefaultAuthenticationSuccessHandler
 {
     protected $logger;
     public function __construct(HttpUtils $httpUtils,array $options = [])
     {
         parent::__construct($httpUtils, $options);
-        // $this->logger = $logger;
-        // parent::__construct($httpUtils, $logger, $options);
     }
     /**
      * {@inheritdoc}
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?Response
     {
-        // dd("さくせすはんどら");
-        // throw new \RuntimeException('Custom SuccessHandler was called!');
-        
-        // file_put_contents('/tmp/login_success.log', date('Y-m-d H:i:s') . " - Login success handler called\n", FILE_APPEND);
-        // $this->logger->info('EccubeLoginSuccessHandler: ログイン成功');
-
         $targetPath = $this->defaultOptions['default_target_path'] ?? '/';
-        // $this->logger->info('Redirecting to: ' . $targetPath);
-
-        // return $this->httpUtils->createRedirectResponse($request, '/admin/');
-
         return $this->httpUtils->createRedirectResponse($request, $targetPath);
     }
 }
