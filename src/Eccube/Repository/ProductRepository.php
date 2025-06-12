e2fb9e1c4bd2db43316dac8769d5c364ee8b0d59
y.honmyo <y.honmyo@zooops-japan.co.jp>
2025-06-12 21:17:34 +0900

    $searchData['orderby']->$orderby

--- src/Eccube/Repository/ProductRepository.php
+++ src/Eccube/Repository/ProductRepository.php
@@ -184,14 +184,14 @@ class ProductRepository extends AbstractRepository
         // 価格低い順
         $config = $this->eccubeConfig;
 
-        // $orderby = $searchData['orderby'] ?? null;
+        $orderby = $searchData['orderby'] ?? null;
 
         // // IDのまま来ている場合はエンティティに変換
         // if (!is_object($orderby) && !empty($orderby)) {
         //     $orderby = $this->productListMaxRepository->find($orderby);
         // }
 
-        if (!empty($searchData['orderby']) && $searchData['orderby']->getId() == $config['eccube_product_order_price_lower']) {
+        if (!empty($orderby) && $orderby->getId() == $config['eccube_product_order_price_lower']) {
             //@see http://doctrine-orm.readthedocs.org/en/latest/reference/dql-doctrine-query-language.html
             $qb->addSelect('MIN(pc.price02) as HIDDEN price02_min');
             $qb->innerJoin('p.ProductClasses', 'pc');
@@ -200,7 +200,7 @@ class ProductRepository extends AbstractRepository
             $qb->orderBy('price02_min', 'ASC');
             $qb->addOrderBy('p.id', 'DESC');
         // 価格高い順
-        } elseif (!empty($searchData['orderby']) && $searchData['orderby']->getId() == $config['eccube_product_order_price_higher']) {
+        } elseif (!empty($orderby) && $orderby->getId() == $config['eccube_product_order_price_higher']) {
             $qb->addSelect('MAX(pc.price02) as HIDDEN price02_max');
             $qb->innerJoin('p.ProductClasses', 'pc');
             $qb->andWhere('pc.visible = true');
@@ -208,7 +208,7 @@ class ProductRepository extends AbstractRepository
             $qb->orderBy('price02_max', 'DESC');
             $qb->addOrderBy('p.id', 'DESC');
         // 新着順
-        } elseif (!empty($searchData['orderby']) && $searchData['orderby']->getId() == $config['eccube_product_order_newer']) {
+        } elseif (!empty($orderby) && $orderby->getId() == $config['eccube_product_order_newer']) {
             // 在庫切れ商品非表示の設定が有効時対応
             // @see https://github.com/EC-CUBE/ec-cube/issues/1998
             //if ($this->getEntityManager()->getFilters()->isEnabled('option_nostock_hidden') == true) {
