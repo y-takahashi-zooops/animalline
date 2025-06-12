6a6a25acddda901158b35bfb6141c7bda216f96f
y.honmyo <y.honmyo@zooops-japan.co.jp>
2025-06-12 11:27:46 +0900

    useの追加

--- src/Eccube/Form/Type/Master/ProductListOrderByType.php
+++ src/Eccube/Form/Type/Master/ProductListOrderByType.php
@@ -19,6 +19,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 // class ProductListOrderByType extends AbstractType
 // {
