cacc985e98b5073e86d8c00a5084e609fd33aa58
MasakiTakahashi <m.takahashi@zooops-japan.co.jp>
2025-06-16 17:40:28 +0900

    自前のログイン処理を呼ぶように改修

--- src/Security/Authenticator/CustomFormLoginAuthenticator.php
+++ src/Security/Authenticator/CustomFormLoginAuthenticator.php
@@ -1,27 +1,28 @@
 <?php
 
-namespace App\Security\Authenticator;
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 
 class CustomFormLoginAuthenticator extends FormLoginAuthenticator
 {
-    protected function getLoginUrl(Request $request): string
-    {
-        return $this->getUrlGenerator()->generate('adoption_login');
+    private HttpUtils $httpUtils;
+    private AuthenticationSuccessHandlerInterface $successHandler;
+    private AuthenticationFailureHandlerInterface $failureHandler;
+
+    public function __construct(
+        HttpUtils $httpUtils,
+        AuthenticationSuccessHandlerInterface $successHandler,
+        AuthenticationFailureHandlerInterface $failureHandler
+    ) {
+        parent::__construct($httpUtils);
+        $this->httpUtils = $httpUtils;
+        $this->successHandler = $successHandler;
+        $this->failureHandler = $failureHandler;
     }
 
-    protected function getDefaultSuccessRedirectUrl(Request $request): string
+    public function getLoginUrl(Request $request): string
     {
-        return '/adoption_mypage';
+        return $this->httpUtils->generateUri($request, 'adoption_login');
     }
 
     public function authenticate(Request $request): Passport
@@ -30,20 +31,23 @@ class CustomFormLoginAuthenticator extends FormLoginAuthenticator
         $password = $request->request->get('login_pass', '');
         $csrfToken = $request->request->get('_csrf_token');
 
-        $request->getSession()->set('_security.last_username', $email);
-
         return new Passport(
-            new \Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge(
-                $email,
-                function ($userIdentifier) use ($password) {
-                    // このクロージャ内で UserProvider 経由で User を取得
-                    return $this->userProvider->loadUserByIdentifier($userIdentifier);
-                }
-            ),
+            new UserBadge($email),
             new PasswordCredentials($password),
             [
                 new CsrfTokenBadge('authenticate', $csrfToken),
+                new RememberMeBadge(),
             ]
         );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return $this->successHandler->onAuthenticationSuccess($request, $token);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        return $this->failureHandler->onAuthenticationFailure($request, $exception);
     }
 }
