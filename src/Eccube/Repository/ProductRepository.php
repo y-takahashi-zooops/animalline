e1f85080e020c40372c4a9db61068974f79072bb
MasakiTakahashi <m.takahashi@zooops-japan.co.jp>
2025-06-17 17:28:21 +0900

    並び替え確認

--- src/Eccube/Repository/ProductRepository.php
+++ src/Eccube/Repository/ProductRepository.php
@@ -184,6 +184,7 @@ class ProductRepository extends AbstractRepository
         $config = $this->eccubeConfig;
 
         $orderby = $searchData['orderby'] ?? null;
+        dd('並び替え確認', $orderby);
 
         // $orderbyがオブジェクトなら getId()、intならそのまま
         $orderbyId = is_object($orderby) && method_exists($orderby, 'getId')
