69281bfe9c6f10f5a3324651699be9c8794e3a78
r.yaegashi <r.yaegashi@zooops-japan.co.jp>
2025-09-11 16:38:52 +0900

    gitignoreの更新とadminのバンドル対応

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'production',
   entry: {
-    front: './html/template/default/assets/js/bundle.js',
-    admin: './html/template/admin/assets/js/bundle.js',
-    install: './html/template/install/assets/js/bundle.js'
+    admin: './html/template/admin/assets/js/bundle.js'
   },
   devtool: 'source-map',
   output: {
