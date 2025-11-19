4f6504fbaa3a5ed6f16647448b57d5050088589f
a.wada <a.wada@zooops-japan.co.jp>
2025-11-19 19:47:53 +0000

    error fix

---
+++ src/Eccube/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Eccube;
+
+/**
+ * Dummy Application class for backward compatibility.
+ *
+ * このクラスは現在の EC-CUBE 4.3/Symfony 6.4 では実質的に使用していませんが、
+ * services.yaml から src/Eccube/* をスキャンする際に、
+ * ファイル名と同名のクラスが存在することを期待されるため定義しています。
+ */
+class Application
+{
+    // ここは空で OK
+}
