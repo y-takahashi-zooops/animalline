479dd28d7ff270e83ae9706989a59d8c4082560f
a.wada <a.wada@zooops-japan.co.jp>
2025-12-03 16:57:54 +0900

    Customize 側に Bundle を作成

---
+++ app/Customize/CustomizeBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Customize;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class CustomizeBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/Resource/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
